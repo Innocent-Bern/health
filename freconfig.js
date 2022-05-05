b86f7ec8a3a6716599bc19cb8384b83c193fc9c8
Innocent_Bern <innocentbern22@gmail.com>
2022-05-05 23:42:34 +0300

    firebasse setup

---
+++ freconfig.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCs8R1Mw26Ap7v1pQmMBzal0uL8URvF048",
+    authDomain: "health-617a8.firebaseapp.com",
+    projectId: "health-617a8",
+    storageBucket: "health-617a8.appspot.com",
+    messagingSenderId: "354618239773",
+    appId: "1:354618239773:web:581885321c2df38f140ff4"
+  };
+
+const app = initializeApp()
