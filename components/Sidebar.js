a5eddca2dec9f3fc399d1fb0a10a61242b732153
Innocent_Bern <innocentbern22@gmail.com>
2022-05-13 01:08:52 +0300

    layout & styling patient homepage

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -6,16 +6,40 @@ import PersonIcon from '@mui/icons-material/Person';
 import HomeIcon from '@mui/icons-material/Home';
 import LogoutIcon from '@mui/icons-material/Logout';
 import { useState } from "react";
-const Sidebar = () => {
+const Sidebar = ({setContentWidth}) => {
     const [toggle, setToggle]= useState(true)
-    const [panel, setPanel]= useState({width: "17vw"})
+    const [panel, setPanel]= useState({width: "20vw"})
     const [btnWidth, setBtnWidth]= useState({width: "80%"})
+    // const [mouse, setMouse]= useState(false)
     // toggle the navigation panel
     const handleClick= ()=>{
         toggle ? setToggle(false) : setToggle(true) 
-        toggle ? setPanel({width: "10vw"}) : setPanel({width: "17vw"})
+        toggle ? setPanel({width: "10vw"}) : setPanel({width: "20vw"})
         toggle ? setBtnWidth({width: "fit-content"}) : setBtnWidth({width: "80%"})
+        toggle ? setContentWidth({width: "90vw"}) : setContentWidth({width: "80vw"})
     }
+    // mouse event styles
+    // let mouseIn={
+    //     width: "80%"
+    //  }
+    //  let mouseOut={
+    //      width: "fit-content"
+    //  }
+    // const handleMouseEvent= (e)=>{
+    //     mouse ? setMouse(false) : setMouse(true) ;
+    //     let btn= document.getElementsByClassName(e.target.classList);
+    //     for(let i = 0; i < btn.length; i++){
+    //         btn[i].addEventListener("mouseenter", (e)=>{
+    //             e.target.classList.add("active")
+    //             console.log("In");
+    //         })
+    //         btn[i].addEventListener("mouseleave", (e)=>{
+    //             e.target.classList.remove("active")
+    //             console.log("In");
+    //         })
+    //     }
+        
+    // }
     return (    
         <nav style={panel} className={styles.navigation_panel}>
             <div style={btnWidth} className={styles.panel_head} >
@@ -25,9 +49,9 @@ const Sidebar = () => {
                 {toggle && <h2 className={styles.panel_name} > Health </h2>}
             </div>
             <div style={btnWidth} className={styles.nav_btn}> <HomeIcon className={styles.icon} /> {toggle && <p>Home</p>} </div>
+            <div style={btnWidth} className={styles.nav_btn}> <PersonIcon className={styles.icon} /> {toggle && <p>Profile</p> }</div>            
             <div style={btnWidth} className={styles.nav_btn}> <AddIcon className={styles.icon} /> {toggle && <p>Book</p>} </div>
             <div style={btnWidth} className={styles.nav_btn}> <WorkHistoryOutlinedIcon className={styles.icon} /> {toggle && <p>History</p>} </div>
-            <div style={btnWidth} className={styles.nav_btn}> <PersonIcon className={styles.icon} /> {toggle && <p>Profile</p> }</div>
             <div style={btnWidth} className={styles.nav_btn}> <LogoutIcon className={styles.icon} /> {toggle && <p>Sign out</p>}</div>
         </nav>        
      );
--- pages/patient/index.js
+++ pages/patient/index.js
@@ -1,25 +1,51 @@
 import Head from "next/head";
 import { auth } from "../../lib/firebase";
+import { onAuthStateChanged } from "firebase/auth";
 import { useState } from "react";
 import styles from "../../styles/Patient.module.css"
 import Sidebar from "../../components/Sidebar";
+import AddIcon from '@mui/icons-material/Add';
+import MenuIcon from '@mui/icons-material/Menu';
+import WorkHistoryOutlinedIcon from '@mui/icons-material/WorkHistoryOutlined';
+import PersonIcon from '@mui/icons-material/Person';
+import HomeIcon from '@mui/icons-material/Home';
+import LogoutIcon from '@mui/icons-material/Logout';
 
 const Patient = () => {
     const [user, setUser]= useState(true)
     const [name, setName]= useState("What's happening baby")
+    const [contentWidth, setContentWidth]= useState({width: "80vw"});
     
-    // Authenticate user
-    const current =auth.currentUser;
-
+    
     return (
         <>
             <Head>
                 <title>Health App | Patient Homepage</title>
             </Head>
             <div className={styles.home_page}>
-                <Sidebar />
-                <div className={styles.content} >
-                    <h1>Welcome {name} </h1>
+                <Sidebar setContentWidth={setContentWidth} />
+                <div style={contentWidth} className={styles.content} >
+                    <div className={styles.top}>
+                        <h1 className={styles.name} >Good Evening Jane</h1>
+                        <div className={styles.date} > <i>12th April</i> </div>
+                    </div>
+                    <div className={styles.items}>
+                        <div className={styles.home_item}>
+                            <div><PersonIcon className={styles.home_icon} /></div>
+                            <h3>View Profile</h3>
+                        </div>
+                        <div className={styles.home_main}> 
+                            <div><AddIcon className={styles.home_icon}/></div>
+                            <h3>Book Appointment</h3>
+                         </div>
+                        <div className={styles.home_item}>
+                            <div><WorkHistoryOutlinedIcon className={styles.home_icon} /></div>
+                            <h3>View History</h3>
+                        </div>
+                    </div>
+                    <div className={styles.bottom}>
+                        <p><i>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi, repellendus! Rem qui voluptas dolorum iure iste harum, blanditiis molestiae, voluptates soluta tenetur incidunt, necessitatibus ullam libero. Quaerat maiores cupiditate cumque?</i> </p>
+                    </div>
                 </div>
             </div>
         </>
